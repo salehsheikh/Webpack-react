84a6bb12b3ee056d3b56e6b36877579a127a1383
saleh sheikh <salehsheikh786@gmail.com>
2024-03-22 01:46:37 +0500

    adding chunks and react loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './App.css';
 import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
+import loadable from 'react-loadable';
+const LoadingComponent=()=><h3>loading wait..</h3>
+const Home=loadable({
+  loader:()=>import(/* webpackChunkName:'HomePage'*/'./Home'),
+  loading:LoadingComponent
+})
+const About=loadable({
+  loader:()=>import(/* webpackChunkName:'AboutPage'*/'./About'),
+  loading:LoadingComponent
+})
 const App = () => {
   return (
     <div>
     <h1 className='colors'><center>hello</center></h1>
+    <div>
+      <button><Link to="/home" >Home</Link></button>
+      <button><Link to="/about" >about</Link></button>
+    </div>
+    <br/>
+    <Routes>
+      <Route path='home'Component={Home}/>
+      <Route path='about' Component={About}/>
+    </Routes>
     <div  className='App-header'>
       <img src={logo} className='App-logo'alt="logo"/>
     </div>
