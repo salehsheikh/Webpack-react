8a97fedce85bcc150a0cad8186aedd59c231d749
saleh sheikh <salehsheikh786@gmail.com>
2024-03-22 01:13:44 +0500

    using html css and style loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    entry:'./src/index.js',
+
+
+    // entry:{
+    //     main:"./src/App.js"
+    // },
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'main.[fullhash].js'
@@ -15,9 +21,37 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
-      }
+      },
+      {
+        test:/\.css$/,
+        use:[
+          "style-loader",
+          "css-loader",
+          // "sass-loader"
+    ]
+      },
+      {
+        test:/\.html$/,
+        use:[
+          {
+            loader:"html-loader"
+          }
+        ]
+      },
+      {
+        test:/\.(svg|png|jpg)$/,
+        use:{
+         loader:'file-loader',
+         options:{
+           name:'[name].[hash].[ext]',
+           outputPath:"images"
+         }
+        }
+       }
     ]
   },
+
+ 
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
